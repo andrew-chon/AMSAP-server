1b76b9e5a4ee5f5fd7358eb3e6dc657b003a9e9a
Andrew Chon <andrewdchon@gmail.com>
2022-02-11 15:14:47 -0600

    Prisma service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '.prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(email: string, password: string): { id; email } {
+    return this.prisma.user.create({ data: { email, password } });
+  }
+}
